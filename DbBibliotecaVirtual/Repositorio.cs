3688becc6ead258f2290d5ec81d23437f48da9b8
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2019-05-07 09:21:15 -0300

    .

---
+++ DbBibliotecaVirtual/Repositorio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DbBibliotecaVirtual
+{
+    public class Repositorio
+    {
+        private ContexBiBliotecaVirtual _contex;
+
+        public Repositorio()
+        {
+            _contex = new ContexBiBliotecaVirtual();
+        }
+        public List<Libros> GetLibros()
+        {
+            List<Libros> ListaLibros = _contex.Libros.ToList();
+            return ListaLibros;
+        }
+    }
+}
